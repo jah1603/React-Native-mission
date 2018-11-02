ee46f87cc6ac6d06498be15cae401c5784a44332
jah1603 <hendersonjamesalexander@gmail.com>
2018-11-02 14:17:09 +0000

    initialises modal container

---
+++ components/ResultsView.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Button, TouchableOpacity, Image } from 'react-native';
+import {ImageBackground} from 'react-native';
+import DatePicker from 'react-native-datepicker';
+import Modal from 'react-native-modal';
